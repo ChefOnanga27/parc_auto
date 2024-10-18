7516892e06590b14db47c9a126ad7f3bd3f5192c
philippe mallet <you@example.com>
2024-10-18 16:41:41 +0200

    version1.0

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+
+
+
+
+
+const prisma = new PrismaClient()
 const app = express()
 const port = 3000
+
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
