51b3703a676b1fb40ba516e92cc1457137399acc
philippe mallet <you@example.com>
2024-10-19 05:08:05 +0200

    version 1.6

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-
-
-
-
-
-
-const app = express()
-const port = 3000
-
-
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
 
 const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 async function connect() {
   try {
@@ -25,12 +18,27 @@ async function connect() {
     console.log('Connexion à la base de données réussie !');
   } catch (error) {
     console.error('Erreur de connexion à la base de données:', error);
+    process.exit(1); // Arrêter le serveur si la connexion échoue
   }
 }
 
+// Connexion à la base de données
 connect();
 
-
+// Démarrer le serveur
 app.listen(port, () => {
-  console.log(`le serveur demarre bien sur ${port}`)
-})
+  console.log(`Le serveur démarre bien sur le port ${port}`);
+});
+
+// Fonction principale pour gérer les erreurs
+async function main() {
+  try {
+    await prisma.$connect(); // Connexion à la base de données
+  } catch (e) {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
